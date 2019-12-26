13afa7399c07588eb46d954f78bd207bb8dddbea
center_commander <langziguilai@yeah.net>
2019-12-26 14:07:41 +0800

    backup setttings

--- app/src/main/java/com/rapider/bloc/fragments/settings/cache/CacheSettingFragment.kt
+++ app/src/main/java/com/rapider/bloc/fragments/settings/cache/CacheSettingFragment.kt
@@ -2,10 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package com.rapider.bloc.fragments.settings.general
+package com.rapider.bloc.fragments.settings.cache
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.rapider.R
@@ -13,22 +14,30 @@ import com.rapider.base.SimplePreferenceFragmentCompat
 import com.rapider.extensions.*
 import mozilla.components.support.base.feature.BackHandler
 
-class GeneralSettingFragment : SimplePreferenceFragmentCompat(),BackHandler {
+class CacheSettingFragment : SimplePreferenceFragmentCompat(),BackHandler {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences_general, rootKey)
+        setPreferencesFromResource(R.xml.preferences_cache, rootKey)
     }
     override fun initView(view: View) {
         super.initView(view)
         view.findViewById<TextView>(R.id.title)?.apply {
-            text = getString(R.string.general)
+            text = getString(R.string.clear_cache)
         }
+
 
     }
     override fun setUpToolbar(toolbar: LinearLayout?) {
         toolbar?.findViewById<View>(R.id.back_icon)?.apply {
-            setOnClickListener{
+            setOnClickListener {
                 onBackPressed()
             }
+        }
+        toolbar?.findViewById<ImageView>(R.id.right_icon)?.apply {
+            setImageResource(R.drawable.ic_trash_22)
+            setOnClickListener {
+                //TODO:clean select data
+            }
+
         }
     }
     override fun onResume() {
@@ -37,11 +46,6 @@ class GeneralSettingFragment : SimplePreferenceFragmentCompat(),BackHandler {
     }
 
     private fun setupPreferences() {
-           findPreferenceById(R.string.pref_key_general_display)?.apply {
-               this.setOnPreferenceClickListener {
 
-                   true
-               }
-           }
     }
 }
